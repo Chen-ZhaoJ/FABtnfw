86bc85d698bf24c1b4aa8af494889fd5a4662300
Zhaojie CHEN(陳昭潔) <zhaojiechen@gamania.com>
2022-08-26 10:28:06 +0800

    initial commit

---
+++ FABtnfw.framework/Headers/FABtnfw.h
@@ -0,0 +1,18 @@
+//
+//  FABtnfw.h
+//  FABtnfw
+//
+//  Created by Zhaojie CHEN(陳昭潔) on 2022/8/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FABtnfw.
+FOUNDATION_EXPORT double FABtnfwVersionNumber;
+
+//! Project version string for FABtnfw.
+FOUNDATION_EXPORT const unsigned char FABtnfwVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FABtnfw/PublicHeader.h>
+
+
